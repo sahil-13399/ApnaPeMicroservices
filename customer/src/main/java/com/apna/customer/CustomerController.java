89edf79ee8bf97187a300973c77cefca2542d183
Sahil Samantaray <sahil13399@gmail.com>
2022-03-26 15:52:32 +0530

    Enabled eureka client for customer service

--- customer/src/main/java/com/apna/customer/CustomerController.java
+++ customer/src/main/java/com/apna/customer/CustomerController.java
@@ -2,6 +2,7 @@ package com.apna.customer;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
