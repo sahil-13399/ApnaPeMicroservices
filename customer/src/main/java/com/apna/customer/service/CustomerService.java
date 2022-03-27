3b758d46020e32de973a1ecb0afa9cb9c67825cb
Sahil Samantaray <sahil13399@gmail.com>
2022-03-27 23:04:53 +0530

    Modified customer service to send notifications

--- customer/src/main/java/com/apna/customer/service/CustomerService.java
+++ customer/src/main/java/com/apna/customer/service/CustomerService.java
@@ -2,23 +2,24 @@ package com.apna.customer.service;
 
 import com.apna.clients.fraud.FraudCheckResponse;
 import com.apna.clients.fraud.FraudClient;
-import com.apna.customer.repository.CustomerRepository;
+import com.apna.clients.notification.NotificationClient;
+import com.apna.clients.notification.NotificationRequest;
+import com.apna.customer.dto.Customer;
+import com.apna.customer.exchanges.CustomerDepositResponse;
 import com.apna.customer.exchanges.CustomerRequest;
 import com.apna.customer.exchanges.TransactionDetailsResponse;
 import com.apna.customer.exchanges.TransactionRequest;
-import com.apna.customer.dto.Customer;
-import com.apna.customer.exchanges.CustomerDepositResponse;
+import com.apna.customer.repository.CustomerRepository;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class CustomerService {
 
   private final CustomerRepository customerRepository;
-  private final RestTemplate restTemplate;
+  private final NotificationClient notificationClient;
   private final FraudClient fraudClient;
 
   public void registerCustomer(CustomerRequest request) {
@@ -42,8 +43,6 @@ public class CustomerService {
     if (fraudCheckResponse.isFraudulentCustomer()) {
       throw new IllegalStateException("fraudster");
     }
-
-    // todo: send notification
 
   }
 
@@ -71,6 +70,17 @@ public class CustomerService {
 
     customerRepository.save(sender);
     customerRepository.save(receiver);
+
+    notificationClient.sendNotification(
+        new NotificationRequest(
+            receiver.getId(),
+            receiver.getFirstName() + " " + receiver.getLastName(),
+            sender.getFirstName() + " " + sender.getLastName(),
+            String.format("Amount %d has been sent from %s to %s",
+                transactionRequest.getTransactionAmount(), sender.getFirstName(),
+                receiver.getFirstName()),
+            transactionRequest.getTransactionAmount()
+        ));
 
     return new TransactionDetailsResponse(sender.getId(), receiver.getId(),
         transactionRequest.getTransactionAmount(),
