f6fedcf788ad16e61eab946a99cc273bf39e5221
Sahil Samantaray <sahil13399@gmail.com>
2022-03-30 23:10:02 +0530

    Tested with asynchronous message delivery

--- notification/src/main/java/com/apna/notification/NotificationApplication.java
+++ notification/src/main/java/com/apna/notification/NotificationApplication.java
@@ -1,6 +1,14 @@
 package com.apna.notification;
 
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class NotificationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NotificationApplication.class, args);
+  }
+
 }
